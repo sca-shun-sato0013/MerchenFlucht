09a19966da3e78199b1230e5b8e3a18cee823277
S.U <sdt22cge0009@edu.sca.ac.jp>
2022-11-14 01:55:12 +0900

    ステージ選択の修正

---
+++ Assets/02_Develop/03_Scripts/02_StageSelectScreen/Select.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnityGameLib;
+
+  public class Select : UnityGameLib,IUnityGameLib
+  {
+      void Start() 
+      {
+            Debug.Log("UnityGameStart");
+      }
+
+      public override void UpdateLib()
+      {
+
+      }
+  }
+    
