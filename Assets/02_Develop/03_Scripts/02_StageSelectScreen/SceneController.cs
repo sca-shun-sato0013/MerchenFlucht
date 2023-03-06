bb54c085c9fa85854e746288b1280c2eaeb4f914
S.U <sdt22cge0009@edu.sca.ac.jp>
2023-03-07 02:49:33 +0900

    赤ずきんの表紙追加、決定ボタンからの遷移を追加

--- Assets/02_Develop/03_Scripts/02_StageSelectScreen/SceneController.cs
+++ Assets/02_Develop/03_Scripts/02_StageSelectScreen/SceneController.cs
@@ -5,23 +5,27 @@ using GameManager;
 
 public class SceneController : MonoBehaviour
 {
+    private int OnSelectButton = 0;
     public void OnAkazukin()
     {
+        OnSelectButton = 1;
         //赤ずきんシーン遷移
         //SceneManager.LoadScene("LittleRedRidingHood");
-        SceneManager.Instance.SceneLoadingAsync("LittleRedRidingHood");
+        //SceneManager.Instance.SceneLoadingAsync("LittleRedRidingHood");
     }
     public void OnHengure()
     {
+        OnSelectButton = 2;
         //ヘン&グレシーン遷移
         //SceneManager.LoadScene("HanselAndGretel");
-        SceneManager.Instance.SceneLoadingAsync("HanselAndGretel");
+        //SceneManager.Instance.SceneLoadingAsync("HanselAndGretel");
     }
-    public void OnPita()
+    public void OnPeter()
     {
+        OnSelectButton = 3;
         //ピーターシーン遷移
         //SceneManager.LoadScene("PeterPan");
-        SceneManager.Instance.SceneLoadingAsync("PeterPan");
+        //SceneManager.Instance.SceneLoadingAsync("PeterPan");
     }
 
     public void OnTitle()
@@ -29,5 +33,24 @@ public class SceneController : MonoBehaviour
         //タイトルシーン遷移
         //SceneManager.LoadScene("title");
         SceneManager.Instance.SceneLoadingAsync("title");
+    }
+
+    public void OnDecideBoutton()
+    {
+        if (OnSelectButton == 1)
+        {
+            Debug.Log("1");
+            SceneManager.Instance.SceneLoadingAsync("LittleRedRidingHood");
+        }
+        if (OnSelectButton == 2)
+        {
+            Debug.Log("2");
+            SceneManager.Instance.SceneLoadingAsync("HanselAndGretel");
+        }
+        if (OnSelectButton == 3)
+        {
+            Debug.Log("3");
+            SceneManager.Instance.SceneLoadingAsync("PeterPan");
+        }
     }
 }
